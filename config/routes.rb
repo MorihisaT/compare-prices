ff3b9459afe2935bea7fe16d63a161fbe0b47c63
MorihisaT <morihisa87go@gmail.com>
2020-09-14 17:59:40 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'users/show'
-  devise_for :users. controllers: {
+  devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  
   root to: 'items#index'
 
   resources :users, only: [:show, :edit, :update] do
