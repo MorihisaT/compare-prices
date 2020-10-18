908262cec81fce78266604823ee562d2ff1ecdb4
MorihisaT <morihisa87go@gmail.com>
2020-10-18 15:29:02 +0900

    子要素のコメントも削除できるようにmodelを編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_many :item_tag_relations
   has_many :tags, through: :item_tag_relations, dependent: :destroy
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def self.search(search, id)
     if search != ""
